850b3adf0edb8a3843fd4b899032fe3dd09dcdc3
Melsara <kaxhortz@gmail.com>
2018-03-06 21:28:27 +0100

    Completing bundle (without drawable).

--- app/src/main/java/escapeadvisor/eu/escaperplaylist/MainActivity.java
+++ app/src/main/java/escapeadvisor/eu/escaperplaylist/MainActivity.java
@@ -2,6 +2,7 @@ package escapeadvisor.eu.escaperplaylist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +13,10 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
     final static String URI_KEY = "uri_key";
+    final static String TITLE_KEY = "title_key";
+    final static String ARTIST_KEY = "artist_key";
+    final static String ALBUM_KEY = "album_key";
+    final static String ART_KEY = "art_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +40,16 @@ public class MainActivity extends Activity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Song song = (Song) adapterView.getItemAtPosition(i);
                 String uri = song.getSongUri();
+                String title = song.getSongTitle();
+                String artist = song.getSongArtist();
+                String album = song.getSongAlbum();
+                Drawable art = song.getSongArt();
                 Intent openSongDetail = new Intent(MainActivity.this, DetailsActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putString(URI_KEY, uri);
+                bundle.putString(TITLE_KEY, title);
+                bundle.putString(ARTIST_KEY, artist);
+                bundle.putString(ALBUM_KEY, album);
                 openSongDetail.putExtras(bundle);
                 startActivity(openSongDetail);
 
