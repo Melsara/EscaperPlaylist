35b087357b54b58b78cbd387fe1ce83c3f30e35a
Melsara <kaxhortz@gmail.com>
2018-03-08 22:03:22 +0100

    Adding songs to play & changing state of the button when playing.

--- app/src/main/java/escapeadvisor/eu/escaperplaylist/DetailsActivity.java
+++ app/src/main/java/escapeadvisor/eu/escaperplaylist/DetailsActivity.java
@@ -3,13 +3,35 @@ package escapeadvisor.eu.escaperplaylist;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailsActivity extends Activity {
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
+import static com.spotify.sdk.android.player.PlayerEvent.kSpPlaybackNotifyPause;
+
+public class DetailsActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    private static final String CLIENT_ID = "cb13233b12cd44ec93ac3bca1cf3296c";
+    private static final String REDIRECT_URI = "escaperplaylist://callback";
+
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1111;
+
+    private Player mPlayer;
     private String uri;
     private String title;
     private String artist;
@@ -19,12 +41,15 @@ public class DetailsActivity extends Activity {
     private TextView titleTextView;
     private TextView artistTextView;
     private TextView albumTextView;
+    private TextView buttonText;
     final static String URI_KEY = "uri_key";
     final static String TITLE_KEY = "title_key";
     final static String ARTIST_KEY = "artist_key";
     final static String ALBUM_KEY = "album_key";
     final static String ART_KEY = "art_key";
     ImageButton playButton;
+    boolean playerIsPlaying = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +63,25 @@ public class DetailsActivity extends Activity {
         album = bundle.getString(ALBUM_KEY);
         art = bundle.getInt(ART_KEY);
         setActivityComponent();
-        playButton = (ImageButton) findViewById(R.id.buttonPlay);
 
         if (playButton != null) {
             playButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    startPlayer();
+
+                    /*Changing state of the play button when pressed*/
+                    playButton.setImageResource(R.drawable.spfy);
+                    buttonText.setText(R.string.playing);
+                    playerIsPlaying = true;
+
+                    /*Launching authentication in Spotify*/
+                    AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                            AuthenticationResponse.Type.TOKEN,
+                            REDIRECT_URI);
+                    builder.setScopes(new String[]{"user-read-private", "streaming"});
+                    AuthenticationRequest request = builder.build();
+
+                    AuthenticationClient.openLoginActivity(DetailsActivity.this, REQUEST_CODE, request);
                 }
             });
 
@@ -64,17 +101,95 @@ public class DetailsActivity extends Activity {
         artistTextView.setText(artist);
         albumTextView = (TextView) findViewById(R.id.textViewAlbum);
         albumTextView.setText(album);
+        playButton = (ImageButton) findViewById(R.id.buttonPlay);
+        buttonText = (TextView) findViewById(R.id.textViewButton);
     }
 
     /**
      * Start playing song (managed by Spotify library)
      */
-    public void startPlayer () {
-        Intent playSong = new Intent(DetailsActivity.this, SpfyPlayer.class);
-        Bundle bundle = new Bundle();
-        bundle.putString(URI_KEY, uri);
-        playSong.putExtras(bundle);
-        startActivity(playSong);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(DetailsActivity.this);
+                        mPlayer.addNotificationCallback(DetailsActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+
+
+    /*The following are methods from the Spotify SDK, handling the playback of the songs.*/
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+
+        mPlayer.playUri(null, uri, 0, 0);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
     }
 
 }
